2695484a81aba352ffbaf394a1891024ac8d1e21
F1L-04 <andreafilipuzzi04@gmail.com>
2025-01-17 16:36:19 +0100

    modifica Load

--- src/main/java/data/Amministratore.java
+++ src/main/java/data/Amministratore.java
@@ -1,26 +1,66 @@
 package data;
 
-public class Amministratore extends Utente {
-    private String username;
+public class Amministratore{
+    private String nome,cognome,email,username,password;
+    private int idA;
 
     // Costruttore
-    public Amministratore(String nome, String cognome, String email, String indirizzo, int eta, String recapitoTelefonico, String citta, String username, int idA) {
-        super(nome, cognome, email, indirizzo, eta, recapitoTelefonico, citta,idA);
-        setUsername(username);
+    public Amministratore(String nome, String cognome, String email, String username, String password, int idA) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.username=username;
     }
 
-    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
-        if (username == null || username.isEmpty()) {
-            throw new IllegalArgumentException("Il nome utente non può essere vuoto.");
-        }
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getIdA() {
+        return idA;
+    }
+
+    public void setIdA(int idA) {
+        this.idA = idA;
+    }
 }
 
 
--- src/main/java/data/Load.java
+++ src/main/java/data/Load.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 public class Load {
 
     public Connection con;
-    private ArrayList<Amministratore> listAmm = null;
 
 
     public Load(){
@@ -20,33 +19,19 @@ public class Load {
         }
     }
 
-    public ArrayList<Amministratore> getListAmm() {
-        return listAmm;
-    }
-
-    public void setListAdminDB(){
+    public ArrayList<Amministratore> getListAdminDB(){
+        ArrayList<Amministratore> listAmm = new ArrayList<>();
         try {
             PreparedStatement ps =
                     con.prepareStatement("SELECT * FROM ADMINN");
             ResultSet rs = ps.executeQuery();
-            this.listAmm = new ArrayList<Amministratore>();
             while (rs.next()){
                 Amministratore a = new Amministratore(rs.getString("Nome"),rs.getString("Cognome"), "gg@hhh", "bb", 2, "gggg", "chcchhch", rs.getString("Username") , rs.getInt("CODICE") );
-            this.listAmm.add(a);
+            listAmm.add(a);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return listAmm;
     }
-
-
-    public static void main(String[] args) {
-        Load l = new Load();
-        l.setListAdminDB();
-        ArrayList<Amministratore> listAdmin = l.getListAmm();
-        for(Amministratore amministratore:listAdmin){
-            System.out.println(amministratore.getUsername() + ' ' + amministratore.getNome() + ' ' + amministratore.getCognome() +'\n');
-        }
-    }
-
 }
